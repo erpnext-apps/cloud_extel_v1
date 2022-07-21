d5086467111c26388f116cba6e7ebc9cb24e87d0
hrishi2003 <hrishifreelance03@gmail.com>
2022-07-21 17:12:53 +0530

    vendor report filters

--- cloud_extel/cloud_extel/report/vendor_report/vendor_report.js
+++ cloud_extel/cloud_extel/report/vendor_report/vendor_report.js
@@ -4,6 +4,38 @@
 
 frappe.query_reports["Vendor Report"] = {
 	"filters": [
+		{
+            fieldname: 'company',
+            label: __('Company'),
+            fieldtype: 'Link',
+            options: 'Company',
+            default: frappe.defaults.get_user_default('company')
+        },
+		{
+            fieldname: 'purchase_order_number',
+            label: __('Purchase Order Number'),
+            fieldtype: 'Link',
+            options: 'Purchase Order',
+        },
+		{
+            fieldname: 'grn_no',
+            label: __('GRN Number'),
+            fieldtype: 'Link',
+            options: 'Purchase Receipt',
+        },
+        {
+            fieldname: 'purchase_invoice_no',
+            label: __('Purchase Invoice number'),
+            fieldtype: 'Link',
+            options: 'Purchase Invoice',
+        },
+		{
+            fieldname: 'supplier_no',
+            label: __('Supplier No'),
+            fieldtype: 'Link',
+            options: 'Supplier',
+        },
+		
 
 	]
 };
